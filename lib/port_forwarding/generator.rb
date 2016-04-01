c1df5e426f7f305814643256d72ecf93054be146
Mircea Samuila <mrcc87@gmail.com>
2016-04-01 21:57:36 +0200

    added generator

--- lib/port_forwarding/generator.rb
+++ lib/port_forwarding/generator.rb
@@ -3,25 +3,16 @@ module PortForwarding
     attr_reader :name
 
     def initialize(file_name)
-      @name = name
+      @name = file_name
     end
     def render_template
-      # Dir.entries("erb/#{path}").each do |file|
-      #   if File.file?("erb/#{path}/#{file}")
-      #     input_file = "erb/#{path}/#{file}"
-      #     renderer = ERB.new(File.read(input_file),0,'-')
-      #     output_file = input_file.gsub(".erb", "").gsub("erb/", "sql/")
-      #     puts "creating #{output_file} ... "
-      #     unless File.directory?("sql/#{path}")
-      #       puts "directory does not exist"
-      #       FileUtils.mkdir_p("sql/#{path}")
-      #     end
-      #     File.open(output_file, "w") do |f|
-      #       f.puts renderer.result(binding)
-      #     end
-      #   end
-      # end
-      puts "#{Dir.pwd}/#{name}"
+      input_file = File.expand_path('../../templates/forward.yml.erb', __FILE__)
+      renderer = ERB.new(File.read(input_file),0,'-')
+      output_file = "#{Dir.pwd}/#{name}.yml"
+      puts "creating #{name}.yml ... "
+      File.open(output_file, "w") do |f|
+        f.puts renderer.result(binding)
+      end
     end
   end
 end
